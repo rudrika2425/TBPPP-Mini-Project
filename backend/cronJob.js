ea95fdc9cd13bae55bf144d9b053792e44d06997
Rudrika Raghav <116345691+rudrika2425@users.noreply.github.com>
2025-02-04 09:20:09 +0530

    cron updates

--- backend/cronJob.js
+++ backend/cronJob.js
@@ -1,33 +1,22 @@
 const cron = require('node-cron');
 const File = require('./models/files');
-const fs = require('fs');
-const path = require('path');
-
 
 const runCronJob = () => {
-  cron.schedule('0 0 * * *', async () => {
+  cron.schedule('0 0 * * *', async () => { 
     try {
       const now = new Date();
       const expiredFiles = await File.find({ expirationDate: { $lt: now } });
 
       if (expiredFiles.length > 0) {
         for (const file of expiredFiles) {
-         
-          fs.unlink(path.join(__dirname, '..', file.path), async (err) => {
-            if (err) {
-              console.log(`Error deleting file: ${file.filename}`, err);
-            } else {
-              
-              await File.deleteOne({ uuid: file.uuid });
-              console.log(`File ${file.filename} deleted successfully.`);
-            }
-          });
+          await File.deleteOne({ uuid: file.uuid }); 
+          console.log(`File record with UUID ${file.uuid} deleted from database.`);
         }
       }
     } catch (err) {
-      console.error('Error cleaning expired files:', err);
+      console.error('Error cleaning expired files from database:', err);
     }
   });
 };
 
-module.exports = runCronJob; 
+module.exports = runCronJob;
